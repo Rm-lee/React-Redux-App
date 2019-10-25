ac0aefaac9542d6ff2a2f44ea3a65193416bd2ee
Roger Lee <rmlee0003@gmail.com>
2019-10-25 10:59:12 -0400

    added condition svg

--- weather-data/src/components/Location.js
+++ weather-data/src/components/Location.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Location(props) {
 let str = props.location.consolidated_weather[0].wind_speed.toString()
+let weatherAbrv = props.location.consolidated_weather[0].weather_state_abbr
  return (
   <div className="location-weather">
    <h2>{props.location.title}</h2>
+
    <p>Conditions: {props.location.consolidated_weather[0].weather_state_name}</p>
+   <img src={`https://www.metaweather.com/static/img/weather/${weatherAbrv}.svg`} />
    <p>Wind Direction: {props.location.consolidated_weather[0].wind_direction_compass}</p>
    <p>Wind Speed: {str.substring(0,5)}</p>
    <p>Min Temp: {parseInt(props.location.consolidated_weather[0].min_temp) * 1.8 + 32}</p>

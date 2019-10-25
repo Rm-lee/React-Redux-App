4990bfcd5825bf5a30fdabc849264bb50c7f247d
Roger Lee <rmlee0003@gmail.com>
2019-10-24 22:05:22 -0400

    created location component to render city's data

--- weather-data/src/actions/weather.js
+++ weather-data/src/actions/weather.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ReadStream } from "tty";
+
 
 export const GET_LOCATION_START = "GET_LOCATION_START";
 export const GET_LOCATION_SUCCESS = "GET_LOCATION_SUCCESS";
@@ -7,8 +7,8 @@ export const GET_LOCATION_ERROR = "GET_LOCATION_ERROR";
 
 
 export function getLocation() {
-const lat = ""
-const long = ""
+let lat = ""
+let long = ""
 
  return dispatch => {
   dispatch({type: GET_LOCATION_START})
@@ -16,10 +16,11 @@ const long = ""
   navigator.geolocation.getCurrentPosition( position => {
    lat = position.coords.latitude
    long = position.coords.longitude
-
+ console.log(lat , long)
  })
+
  axios
- .get(`https://www.metaweather.com/api/location/search/?lattlong=${lat},${long}`)
+ .get(`https://www.metaweather.com/api/location/search/?lattlong=${lat.toString()},${long.toString()}`)
  .then(res => {
   dispatch({type: GET_LOCATION_SUCCESS, payload: res.data})
  })
--- weather-data/src/components/WeatherDisplay.js
+++ weather-data/src/components/WeatherDisplay.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useEffect}from 'react';
+import {connect} from "react-redux"
+import {getLocation} from "../actions/weather"
 
-const WeatherDisplay = (props) => {
- navigator.geolocation.getCurrentPosition( position => {
-  
 
-  console.log(position.coords.latitude)
-})
+function WeatherDisplay(props){
+
+ useEffect(() => {
+  props.getLocation()
+ },[props])
+
  return (
   <div>
    
   </div>
  );
 };
-
-export default WeatherDisplay;
+function mapStateToProps(state) {
+return {
+ isLoading: state.weather.isLoading,
+ weatherData: state.weather.weatherData,
+ error: state.weather.error
+}
+}
+const mapDispatchToProps = {
+getLocation
+}
+export default connect(
+ mapStateToProps,
+ mapDispatchToProps
+ )(WeatherDisplay);
